4e4399eff6f145f7a95245b9a456be6c8288944d
ArtyomKondratovich <kondratovich443@gmail.com>
2024-02-21 19:38:57 +0300

    Add project files.

---
+++ TestTask/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using TestTask.Enums;
+
+namespace TestTask.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public UserStatus Status { get; set; }
+
+        public virtual List<Order> Orders { get; set; }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Enums;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            return await _context.Users
+                .GroupJoin(
+                _context.Orders,
+                u => u.Id,
+                o => o.UserId,
+                (u, orders) => new
+                {
+                    u.Id,
+                    u.Email,
+                    u.Status,
+                    OrdersCount = orders.Count()
+                })
+                .OrderByDescending(x => x.OrdersCount)
+                .Select(x => new User()
+                {
+                    Id = x.Id,
+                    Email = x.Email,
+                    Status = x.Status
+                })
+                .FirstAsync();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _context.Users
+                .Where(x => x.Status == UserStatus.Inactive)
+                .ToListAsync();
+        }
+    }
+}
